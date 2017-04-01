2997253b18a6629af3b3c4a194f483d7fd096c6b
solodartem <solod_a@ukr.net>
2017-04-01 10:07:14 +0300

    Init

--- src/main/java/com/mkyong/web/controller/HelloController.java
+++ src/main/java/com/mkyong/web/controller/HelloController.java
@@ -2,27 +2,18 @@ package com.mkyong.web.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.ModelAndView;
+
+import java.util.Map;
 
 @Controller
 public class HelloController {
 
 	@RequestMapping(value = "/auth", method = RequestMethod.GET)
 	@ResponseBody
-    public String get(ModelMap model) {
-        System.out.println("Invoke of GET method");
-        return "hello";
-
-	}
-
-	@RequestMapping(value = "/auth", method = RequestMethod.POST)
-	@ResponseBody
-    public String post(ModelMap model) {
-        System.out.println("Invoke of GET method");
+    public String get(@RequestParam Map<String,String> allRequestParams) {
+        System.out.println("Invoke of GET method with parameters: " + allRequestParams);
         return "hello";
 
 	}
